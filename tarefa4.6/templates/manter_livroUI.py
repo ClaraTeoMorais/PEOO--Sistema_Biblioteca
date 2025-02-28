0480de1fb944a499097d7fd2a7a45e4eb34b0df0
Amanda Lara Duarte da Costa <161068129+Amandalara@users.noreply.github.com>
2025-02-28 09:48:49 -0300

    Falta só quantidade!!

--- tarefa4.6/templates/manter_livroUI.py
+++ tarefa4.6/templates/manter_livroUI.py
@@ -14,96 +14,84 @@ class Manter_Livro_UI:
         with tab2: Manter_Livro_UI.inserir()
         with tab3: Manter_Livro_UI.atualizar()
         with tab4: Manter_Livro_UI.excluir()
-    	
     
     def listar():
         livros = View.livro_listar()
-        if len(livros) == 0: 
+        if not livros:
             st.write("Nenhum livro cadastrado")
-        else:
-            d = []
-            for obj in livros:
-                obj_formatado = obj
-                # Buscar o nome do gênero pelo ID 
-                genero_obj = Generos.listar_id(obj.get_id_genero())
-                nome_genero = genero_obj.get_genero() if genero_obj else "Gênero não encontrado"
-                
-                # Atualizar o objeto formatado com o nome do gênero
-                obj_formatado.set_genero(nome_genero)
-                
-                # Adicionar o dicionário do objeto com o nome do gênero
-                d.append(obj_formatado.__dict__)
-            
-            dic = [{
+            return
+
+        dados = []
+        for livro in livros:
+            genero_obj = Generos.listar_id(livro.get_id_genero())
+            nome_genero = genero_obj.get_genero() if genero_obj else "Gênero não encontrado"
+            dados.append({
                 "Título": livro.get_livro(),
                 "Autor": livro.get_autor(),
-                "Gênero": livro.get_id_genero(),
+                "Gênero": nome_genero,
                 "Editora": livro.get_editora(),
                 "Ano": livro.get_ano(),
                 "Quantidade": livro.get_quantidade(),
                 "Disponível": livro.get_quantDisponivel()
-            } for livro in livros]
-
-            df = pd.DataFrame(dic)
-            st.dataframe(df)
-
+            })
+        df = pd.DataFrame(dados)
+        st.dataframe(df)
+    
     def inserir():
         generos = View.genero_listar()
         livros = st.text_input("Informe o nome do livro")
         autor = st.text_input("Informe o autor do livro")
-        genero = st.selectbox("Informe o gênero do livro", generos, index = None)
+        genero = st.selectbox("Informe o gênero do livro", generos, format_func=lambda x: x.get_genero(), index=None)
         editora = st.text_input("Informe a editora do livro")
         ano = st.number_input("Informe o ano que o livro foi publicado", step=1, value=2000, max_value=2025)
-        quantidade = st.number_input("Informe o quantidade de livros", step=1, min_value=0)
-        quantDisponivel = st.number_input("Informe o quantidade de livros disponíveis", step=1, min_value=0)
+        quantidade = st.number_input("Informe a quantidade de livros", step=1, min_value=0)
+        quantDisponivel = st.number_input("Informe a quantidade de livros disponíveis", step=1, min_value=0)
 
         if st.button("Inserir"):
-
-            id_Genero = None
-            if genero != None: id_Genero = genero.get_id()
-            if livros == "":
-                st.session_state["placeholder"] = "O campo não pode estar vazio!"
+            id_Genero = genero.get_id() if genero else None
+            if not livros:
                 st.error("Informe o livro")
                 time.sleep(2)
                 st.rerun()
-            
             else:
                 View.livro_inserir(livros, autor, id_Genero, editora, ano, quantidade, quantDisponivel)
-                st.success("livro inserido com sucesso")
+                st.success("Livro inserido com sucesso")
                 time.sleep(2)
                 st.rerun()
-        
+    
     def atualizar():
         livros = View.livro_listar()
-        if len(livros) == 0: 
+        if not livros:
             st.write("Nenhum livro cadastrado")
-        else:
-            op = st.selectbox("Atualização de livro", livros)
-            generos = View.genero_listar()
-            livro = st.text_input("Informe o novo nome do livro", op.get_livro())
-            autor = st.text_input("Informe o novo autor do livro", op.get_autor())
-            genero = st.selectbox("Informe o novo gênero do livro", generos, index = None)
-            editora = st.text_input("Informe a nova editora do livro", op.get_editora())
-            ano = st.text_input("Informe o novo ano que o livro foi publicado", op.get_ano())
-            quantidade = (st.text_input("Informe a nova quantidade de livros", op.get_quantidade()))
-            quantDisponivel = (st.text_input("Informe a nova quantidade de livros disponíveis"))
+            return
 
-            if st.button("Atualizar"):
-                id_Genero = None
-                if genero != None: id_Genero = genero.get_id()
-                View.livro_atualizar(op.get_id(), livro, autor, id_Genero, editora, ano, quantidade, quantDisponivel)
-                st.success("livro atualizado com sucesso")
-                time.sleep(2)
-                st.rerun()
+        op = st.selectbox("Atualização de livro", livros, format_func=lambda x: x.get_livro())
+        generos = View.genero_listar()
+        livro = st.text_input("Informe o novo nome do livro", op.get_livro())
+        autor = st.text_input("Informe o novo autor do livro", op.get_autor())
+        genero = st.selectbox("Informe o novo gênero do livro", generos, format_func=lambda x: x.get_genero(), index=None)
+        editora = st.text_input("Informe a nova editora do livro", op.get_editora())
+        ano = st.number_input("Informe o novo ano que o livro foi publicado", step=1, value=int(op.get_ano()), max_value=2025)
+        quantidade = st.number_input("Informe a nova quantidade de livros", step=1, min_value=0, value=int(op.get_quantidade()))
+        quantDisponivel = st.number_input("Informe a nova quantidade de livros disponíveis", step=1, min_value=0, value=int(op.get_quantDisponivel()))
 
+        if st.button("Atualizar"):
+            id_Genero = genero.get_id() if genero else None
+            View.livro_atualizar(op.get_id(), livro, autor, id_Genero, editora, ano, quantidade, quantDisponivel)
+            st.success("Livro atualizado com sucesso")
+            time.sleep(2)
+            st.rerun()
+    
     def excluir():
         livros = View.livro_listar()
-        if len(livros) == 0: 
+        if not livros:
             st.write("Nenhum livro cadastrado")
-        else:
-            op = st.selectbox("Exclusão de livro", livros)
-            if st.button("Excluir"):
-                View.livro_excluir(op.get_id())
-                st.success("livro excluído com sucesso")
-                time.sleep(2)
-                st.rerun() 
+            return
+
+        op = st.selectbox("Exclusão de livro", livros, format_func=lambda x: x.get_livro())
+
+        if st.button("Excluir"):
+            View.livro_excluir(op.get_id())
+            st.success("Livro excluído com sucesso")
+            time.sleep(2)
+            st.rerun()
